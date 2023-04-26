9c6e0e07d707f6a2f6ba85f1362157a700f955fe
marina melgar <marina.melgar13@gmail.com>
2023-04-26 13:40:21 +0200

    Début page acceuil

---
+++ wp-content/themes/astra-child/templates/template-accueil.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template Name: template accueil
+ * Template Post Type: post, page, product
+ */
+
+
+get_header(); ?>
+<main>
+<h1>BOISSON ÉNERGISANTE <br>100% NATURELLE</h1>
+
+<div class="canette-feuille">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/feuille-1.png" alt="feuille"  >
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/canette.png" alt="canette" >
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/feuille-2.png" alt="feuille" >
+</div>
+<section class="plante">
+<h2>L'énergie des plantes</h2>
+</section>
+</main>
+
+<?php get_footer();
