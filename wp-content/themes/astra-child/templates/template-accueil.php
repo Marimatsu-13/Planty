3e184334e37b2cea22e2e01acbbaa3c69148a753
marina melgar <marina.melgar13@gmail.com>
2023-04-27 10:50:22 +0200

    Fin de la deuxième section accueil

--- wp-content/themes/astra-child/templates/template-accueil.php
+++ wp-content/themes/astra-child/templates/template-accueil.php
@@ -7,15 +7,23 @@
 
 get_header(); ?>
 <main>
-<h1>BOISSON ÉNERGISANTE <br>100% NATURELLE</h1>
+<section class="bordure">
+    <h1>BOISSON ÉNERGISANTE <br>100% NATURELLE</h1>
+
 
 <div class="canette-feuille">
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/feuille-1.png" alt="feuille"  >
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/canette.png" alt="canette" >
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/feuille-2.png" alt="feuille" >
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/canette.png" alt="canette" class="canette">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/feuille-1.png" alt="feuille">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/feuille-2.png" alt="feuille">
 </div>
+</section>
 <section class="plante">
-<h2>L'énergie des plantes</h2>
+    <h2>L'énergie des plantes</h2>
+    <p> Planty, c’est la première boisson<br>
+    énergisante composée à 100 % de produits naturels.<br> 
+    Avec son goût frais et fruité, retrouvez votre <br>
+    énergie grâce aux plantes riches en vitamine B2, <br>
+    à n’importe quel moment de la journée.</p>
 </section>
 </main>
 
