1105a678d2ead77f6811a21642d08f6c430baa5b
marina melgar <marina.melgar13@gmail.com>
2023-05-15 12:26:40 +0200

    RESPONSIVE NOUS RENCONTRER

---
+++ wp-content/themes/blankslate-child/templates/template-nous-rencontrer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: template nous rencontrer
+ * Template Post Type: post, page, product
+ */
+
+get_header(); ?>
+<main>
+<section class="bordure3">
+<?php the_content(); ?>
+</section>
+<section class="presentation">
+</section>
+<section class="form">
+</section>
+</main>
+
+<?php get_footer();
