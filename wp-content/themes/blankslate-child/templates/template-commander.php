dd59b7b4acf86337eff815fe1c08f0adc1fcff10
marina melgar <marina.melgar13@gmail.com>
2023-05-16 15:57:16 +0200

    fin form commande

---
+++ wp-content/themes/blankslate-child/templates/template-commander.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template Name: template commander
+ * Template Post Type: post, page, product
+ */
+
+get_header(); ?>
+<main>
+<section class="bordure4">
+<?php the_content(); ?>
+</section>
+</main>
+
+<?php get_footer();
