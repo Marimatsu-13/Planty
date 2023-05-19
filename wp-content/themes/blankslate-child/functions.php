08e6f6b3cca3d3b632f2e55d6bde4e7c0d1ebeee
marina melgar <marina.melgar13@gmail.com>
2023-05-19 11:27:51 +0200

    admin hide v1

--- wp-content/themes/blankslate-child/functions.php
+++ wp-content/themes/blankslate-child/functions.php
@@ -5,5 +5,19 @@ function theme_enqueue_styles() {
 	wp_enqueue_style('child-theme', get_stylesheet_directory_uri() .'/style.css', array('parent-theme'));
 }
 
+function my_wp_nav_menu_items( $items = '' ) {
+    if( is_user_logged_in() ) {
+    // Logged in menu to display
+    $items['menu'] = 5;
+     
+    } else {
+    // Non-logged-in menu to display
+    $items['menu'] = 25;
+    }
+    return $items;
+    }
+    add_filter( 'wp_nav_menu_args', 'my_wp_nav_menu_items' );
+
+
 
 
